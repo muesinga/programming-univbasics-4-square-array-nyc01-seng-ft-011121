95f8ff09a89fcd96f75621f31ef887cb6bae1ea8
Aaron Muesing <muesing.music@gmail.com>
2020-12-24 20:24:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   count = 0
   new_array = []
   while count < numbers.length do
-    numbers.index ** 2 
+    new_array.push numbers.index ** 2 
   end
   square_array(numbers)
 end 
