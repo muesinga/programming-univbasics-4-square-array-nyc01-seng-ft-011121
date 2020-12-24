c4cd327cb96ce751e0bef1fd48cf808d2a09d5ef
Aaron Muesing <muesing.music@gmail.com>
2020-12-24 20:09:33 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,2 @@
 def square_array(array)
-  count = 0
-  new_array = []
-  array.length.times do |index|
-  new_array.push(array[index] **)
-end
-new_array
   
-  
-  while count < array.length do
-    maths.exp
-end
-end
